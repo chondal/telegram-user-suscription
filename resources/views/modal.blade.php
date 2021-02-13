a77a600355f2faf583d50cf6a6ebb724a6f85144
Jonathan <jg.pradi@gmail.com>
2021-02-13 16:00:44 -0300

    modal ok, listo para primera prueba

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -8,20 +8,20 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Recibí notificaciones en Telegram.</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-12 text-center">
                         <p>Escaneá el código con tu celular para hablar con el Bot y te llegarán las notificaciones en tu celular.</p>
                         <div class="m-2">
                             {{ TelegramUserSuscription::qr(200) }}
                         </div>
                         <p>También podes acceder desde este link.</p>
-                        <a href="{{ TelegramUserSuscription::link() }}"> <img src="{{ config('telegram-user.icon') }}" alt=""> ACCEDER</a>
+                        <a class="btn btn-link" href="{{ TelegramUserSuscription::link() }}"> <img width="20px" src="{{ config('telegram-user.icon') }}" alt=""> ACCEDER AHORA</a>
                     </div>
                 </div>
             </div>
